510ced56cf623ef49f303beb8e26fd1073dda66b
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-24 23:07:47 +0100

    loading

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
 import React, { useState, useEffect } from "react";
 import Tile from "./component/Card";
+import Loading from "./component/Loading";
 import { Grid } from "@mui/material";
 import "./index.css";
 
 function App() {
   const [tour, setTour] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
+  async function fetchTour() {
+    setLoading(true);
     const url = "http://course-api.com/react-tours-project";
     try {
-      async function tourdata() {
-        const res = await fetch(url);
-        const data = await res.json();
-        setTour(data);
-      }
-      tourdata();
+      const res = await fetch(url);
+      const data = await res.json();
+      setLoading(false);
+      setTour(data);
     } catch (err) {
       console.error(err.message);
     }
+  }
+
+  useEffect(() => {
+    fetchTour();
   }, []);
 
-  function deleteTour(e){
-      const element = e.target
-      console.log(element)
+  function deleteTour(id) {
+    const newTour = tour.filter((t) => t.id !== id);
+    setTour(newTour);
   }
 
   const eachTour = tour.map((t) => {
-    return <Tile key={t.id} item={t} delete={deleteTour}/>;
+    return <Tile key={t.id} item={t} delete={deleteTour} />;
   });
+
+
+  if (loading) {
+    return (
+      <div className="app-con">
+        <h2 className="title">Tour</h2>
+        <div className="tour-display"><Loading/></div>
+      </div>
+    );
+  }
+  
+  if (tour.length === 0) {
+    return (
+      <>
+        <div className="app-con">
+          <h2 className="title">Tour</h2>
+          {/* <Loading/> */}
+          <div className="tour-refresh">No more tours. kindly refresh</div>
+          <button className="btn-refresh" onClick={fetchTour}>
+            {" "}
+            refresh
+          </button>
+        </div>
+      </>
+    );
+  }
+
+ 
 
   return (
     <div className="app-con">
-    <h2 className="title">Tour</h2>
-      <div className="tour-display">{eachTour}
-      </div>
-    </div> 
+      <h2 className="title">Tour</h2>
+      <div className="tour-display">{eachTour}</div>
+    </div>
   );
 }
 
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,36 +1,56 @@
-import React, {useState, useEffect} from "react";
-import { Card, CardContent, CardMedia, Typography, Button, CardActionArea, CardActions } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+  CardActionArea,
+  CardActions,
+} from "@mui/material";
 
 export default function Tile(props) {
-console.log(props)
+  const [readMore, setRead] = useState(false);
 
-const [readMore, setRead] = useState(true)
-
-function toggle(){
-    setRead(prev => !prev)
-    console.log(readMore)
-}
-  return (<>
-      <Card sx={{ maxWidth: 345, m:5}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={props.item.image}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.item.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          {readMore ? props.item.info : `${props.item.info.substring(0,200)}...`}<button onClick={()=>setRead(!readMore)}>{readMore ? "show less" : "see more"}</button>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions sx={{display:"flex", justifyContent:'center'} }>
-        <Button variant="outlined" color='error' onClick={props.delete}>Not Interested</Button>
-      </CardActions>
-    </Card>
-  </>);
+  function toggle() {
+    setRead((prev) => !prev);
+    console.log(readMore);
+  }
+  return (
+    <>
+      <Card sx={{ maxWidth: 345, m: 5 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={props.item.image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {props.item.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {readMore
+                ? props.item.info
+                : `${props.item.info.substring(0, 200)}...`}
+              <button onClick={() => setRead(!readMore)} className="textBtn">
+                {readMore ? "show less" : "see more"}
+              </button>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            variant="outlined"
+            color="error"
+            onClick={() => props.delete(props.item.id)}
+            sx={{ mb: 4 }}
+          >
+            Not Interested
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  );
 }
